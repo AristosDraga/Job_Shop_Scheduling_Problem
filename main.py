560bedc653e399d7fb29b3937aa8f1ef4a1540d5
Aristos Dragatis <64551622+AristosDraga@users.noreply.github.com>
2024-01-14 15:20:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import read_files
+# Aristeidis Dragatis - AM : 2111
+# main file that runs the whole code 
+import read_files, spt_rule
 
 if __name__ == '__main__':
-    read_files()
+    read_files
+    spt_rule
